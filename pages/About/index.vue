adecee2522d53e30392f126a1dc5a755c84a4a7b
iamziamz <zizo@zizo-pelling.com>
2019-09-30 22:40:54 +0200

    My First Commit

---
+++ pages/About/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>About DadJokes</h1>
+      <p>This is app that display corny Dadjokes</p>
+  </div>  
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title:'About The App',
+            meta: [
+                {
+                    hid: "description",
+                    name:"description",
+                    content:"best place for corny dad jokes"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
